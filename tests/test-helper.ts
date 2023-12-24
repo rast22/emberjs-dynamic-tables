82fea1c8f23a85bb07b5cf4f131645dd8edf3df9
Iaroslav Paramonov <iaroslav.paramonov@positiva.si>
2023-12-24 01:04:01 +0100

    feat: Initialized new Ember.js app

---
+++ tests/test-helper.ts
@@ -0,0 +1,12 @@
+import Application from 'emberjs-dynamic-tables/app';
+import config from 'emberjs-dynamic-tables/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
