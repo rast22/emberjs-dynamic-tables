8cbceae41db39c76386a1b5b791de3104879f086
Iaroslav Paramonov <iaroslav.paramonov@positiva.si>
2024-01-12 09:57:22 +0100

    feat: Implemented dynamic columns select.

--- app/components/main-table/component.ts
+++ app/components/main-table/component.ts
@@ -5,23 +5,22 @@ import { action } from '@ember/object';
 import Store from '@ember-data/store';
 import { inject as service } from '@ember/service';
 import type KeywordModel from 'emberjs-dynamic-tables/models/keyword';
-import type NotificationService from "emberjs-dynamic-tables/services/notification";
+import type NotificationService from 'emberjs-dynamic-tables/services/notification';
 
 export default class MainTableComponent extends Component {
   // data variables
   @tracked tableData: any = [];
   @service store!: Store;
-  @tracked competitors = new Map<
-    string,
-    { rank: number | null; rank_type: string | null }
-  >();
-
+  // @tracked competitors = new Map<
+  //   string,
+  //   { rank: number | null; rank_type: string | null }
+  // >();
   // component state variables
   @tracked isLoading = false;
   @tracked loadError: boolean = false;
 
   // notifications variables
-  @service('notification') notificationService!: NotificationService ;
+  @service('notification') notificationService!: NotificationService;
 
   // pagination variables
   @tracked currentPage = 1;
@@ -30,6 +29,10 @@ export default class MainTableComponent extends Component {
   @tracked totalPages = 0;
   @tracked currentRows = 0;
 
+  // toolbar variables
+  @tracked competitors: Array<string> = [];
+  @tracked selectedCompetitor: number  = 0;
+  @tracked allowedCols: Array<{name: string, alias: string, selected: boolean, sort: number}> = [];
   constructor(owner: Owner, args: any) {
     super(owner, args);
     this.loadKeywordsResults();
@@ -39,7 +42,11 @@ export default class MainTableComponent extends Component {
   async loadKeywordsResults() {
     this.isLoading = true;
     try {
-      const response = await this.store.findAll('keyword');
+      const response = await this.store.query('keyword', {});
+
+      this.competitors = response['meta']['globalCompetitors']
+      this.allowedCols = response['meta']['allowedColumns']
+
       this.tableData = response;
       this.innitPagination();
       this.notificationService.show('Data successfully fetched', 'success');
@@ -47,7 +54,10 @@ export default class MainTableComponent extends Component {
     } catch (e) {
       console.error(e);
       this.loadError = true;
-      this.notificationService.show('Error fetching data, please reload the page or click the reload button', 'error');
+      this.notificationService.show(
+        'Error fetching data, please reload the page or click the reload button',
+        'error',
+      );
     } finally {
       this.isLoading = false;
     }
@@ -81,92 +91,121 @@ export default class MainTableComponent extends Component {
   }
 
   /**
-   *  Select and get a  competitor for a given keyword.
+   *  Select a competitor.
    * @returns {Promise<void>}
    * @param event
    */
   @action
   async selectCompetitor(event: Event): Promise<void> {
-    const target = event.target as HTMLSelectElement;
-    const keyword_id = target.id;
-    const competitor_id = target.value;
+    this.selectedCompetitor = (event.target as HTMLSelectElement).value;
+    // const target = event.target as HTMLSelectElement;
+    // const keyword_id = target.id;
+    // const competitor_id = target.value;
+    //
+    // // Use a composite key for each competitor data
+    // const compositeKey = `${keyword_id}_${competitor_id}`;
+    //
+    // try {
+    //   // Check if we already have the competitor data
+    //   if (!this.competitors.has(compositeKey)) {
+    //     const competitorData = await this.store.queryRecord('competitor', {
+    //       keyword_id: keyword_id,
+    //       competitor: competitor_id,
+    //     });
+    //
+    //     // Find keyword to update selected competitor state
+    //     const keywordToUpdate = this.tableData.find(
+    //       (keyword: KeywordModel) => keyword.id === keyword_id,
+    //     );
+    //     if (keywordToUpdate) {
+    //       keywordToUpdate.selected_competitor = competitor_id;
+    //
+    //       // Add competitors to map
+    //       this.competitors.set(compositeKey, {
+    //         rank: competitorData.rank,
+    //         rank_type: competitorData.rank_type,
+    //       });
+    //     }
+    //   } else {
+    //     const keywordToUpdate = this.tableData.find(
+    //       (keyword: KeywordModel) => keyword.id === keyword_id,
+    //     );
+    //     if (keywordToUpdate) {
+    //       keywordToUpdate.selected_competitor = competitor_id;
+    //     }
+    //   }
+    //   this.notificationService.show('Data successfully fetched', 'success');
+    // } catch (e) {
+    //   console.error('Error fetching competitor data:', e);
+    //   this.notificationService.show(
+    //     'Error fetching competitors data, please select another one',
+    //     'error',
+    //   );
+    // }
+  }
 
-    // Use a composite key for each competitor data
-    const compositeKey = `${keyword_id}_${competitor_id}`;
+  // /**
+  //  * Get the competitor rank for a given keyword.
+  //  * @param keyword
+  //  * @returns {number | string}
+  //  */
+  // @action
+  // getCompetitorRank(keyword: KeywordModel): number | string {
+  //   const compositeKey = `${keyword.id}_${keyword.selected_competitor}`;
+  //
+  //   const competitorData = this.competitors.get(compositeKey);
+  //   if (competitorData) {
+  //     // Access the rank property
+  //     const rank = competitorData.rank;
+  //     return rank ?? 'No data for this competitor';
+  //   }
+  //
+  //   return 'No data for this competitor';
+  // }
+  //
+  // /**
+  //  * Get the competitor rank type for a given keyword.
+  //  * @param keyword
+  //  * @returns {string}
+  //  */
+  // @action
+  // getCompetitorRankType(keyword: KeywordModel): string {
+  //   const compositeKey = `${keyword.id}_${keyword.selected_competitor}`;
+  //
+  //   const competitorData = this.competitors.get(compositeKey);
+  //   if (competitorData) {
+  //     // Access the rank property
+  //     const rank = competitorData.rank_type;
+  //     return rank ?? 'No data for this competitor';
+  //   }
+  //
+  //   return 'No data for this competitor';
+  // }
 
-    try {
-      // Check if we already have the competitor data
-      if (!this.competitors.has(compositeKey)) {
-        const competitorData = await this.store.queryRecord('competitor', {
-          keyword_id: keyword_id,
-          competitor: competitor_id,
-        });
-
-        // Find keyword to update selected competitor state
-        const keywordToUpdate = this.tableData.find(
-          (keyword: KeywordModel) => keyword.id === keyword_id,
-        );
-        if (keywordToUpdate) {
-          keywordToUpdate.selected_competitor = competitor_id;
-
-          // Add competitors to map
-          this.competitors.set(compositeKey, {
-            rank: competitorData.rank,
-            rank_type: competitorData.rank_type,
-          });
-        }
-      } else {
-        const keywordToUpdate = this.tableData.find(
-          (keyword: KeywordModel) => keyword.id === keyword_id,
-        );
-        if (keywordToUpdate) {
-          keywordToUpdate.selected_competitor = competitor_id;
-        }
+  /**
+   * Toggles a column.
+   * @param columnAlias
+   */
+  @action
+  toggleColumn(columnAlias: string) {
+    let shownColumns = this.allowedCols.map(column => {
+      if (column.alias === columnAlias) {
+        return { ...column, selected: !column.selected };
       }
-      this.notificationService.show('Data successfully fetched', 'success');
-    } catch (e) {
-      console.error('Error fetching competitor data:', e);
-      this.notificationService.show('Error fetching competitors data, please select another one', 'error');
-
-    }
+      return column;
+    });
+    shownColumns = shownColumns.sort((a, b) => a.sort - b.sort);
+    this.allowedCols = shownColumns;
   }
 
   /**
-   * Get the competitor rank for a given keyword.
-   * @param keyword
-   * @returns {number | string}
+   * Get the data for a given cell.
+   * @param dataItem
+   * @param column
    */
   @action
-  getCompetitorRank(keyword: KeywordModel): number | string {
-    const compositeKey = `${keyword.id}_${keyword.selected_competitor}`;
-
-    const competitorData = this.competitors.get(compositeKey);
-    if (competitorData) {
-      // Access the rank property
-      const rank = competitorData.rank;
-      return rank ?? 'No data for this competitor';
-    }
-
-    return 'No data for this competitor';
-  }
-
-  /**
-   * Get the competitor rank type for a given keyword.
-   * @param keyword
-   * @returns {string}
-   */
-  @action
-  getCompetitorRankType(keyword: KeywordModel): string {
-    const compositeKey = `${keyword.id}_${keyword.selected_competitor}`;
-
-    const competitorData = this.competitors.get(compositeKey);
-    if (competitorData) {
-      // Access the rank property
-      const rank = competitorData.rank_type;
-      return rank ?? 'No data for this competitor';
-    }
-
-    return 'No data for this competitor';
+  getCellData(dataItem: KeywordModel, column: { alias: number }) {
+    return dataItem.competitors && dataItem.competitors[this.selectedCompetitor][column.alias];
   }
 
   innitPagination() {
--- mirage/config.js
+++ mirage/config.js
@@ -21,31 +21,19 @@ export default function (config) {
 function routes() {
   this.namespace = '/api';
 
-  // defining a route handler for GET /api/keywords
+  // defining a route handler for GET /api/competitors
   this.get('/competitors', (schema, request) => {
     // Random errors
 
-      let keyword_id = request.queryParams.keyword_id;
-      let competitor_id = request.queryParams.competitor;
+    let keyword_id = request.queryParams.keyword_id;
+    let competitor_id = request.queryParams.competitor;
 
-      const keywordCompetitorsList = schema.competitors.find(keyword_id);
-      let competitorData =
-        keywordCompetitorsList.competitor_results[competitor_id];
-      if (ENV.mirageConfig.enableRandomErrors) {
-        if (Math.random() > 0.5) {
-          return new Response(500, {}, { errors: ['Server Error'] });
-        } else {
-          return {
-            data: {
-              type: 'competitor',
-              id: competitor_id,
-              attributes: {
-                rank: competitorData.rank,
-                rank_type: competitorData.rank_type,
-              },
-            },
-          };
-        }
+    const keywordCompetitorsList = schema.competitors.find(keyword_id);
+    let competitorData =
+      keywordCompetitorsList.competitor_results[competitor_id];
+    if (ENV.mirageConfig.enableRandomErrors) {
+      if (Math.random() > 0.5) {
+        return new Response(500, {}, { errors: ['Server Error'] });
       } else {
         return {
           data: {
@@ -57,6 +45,18 @@ function routes() {
             },
           },
         };
+      }
+    } else {
+      return {
+        data: {
+          type: 'competitor',
+          id: competitor_id,
+          attributes: {
+            rank: competitorData.rank,
+            rank_type: competitorData.rank_type,
+          },
+        },
+      };
     }
   });
 
@@ -66,11 +66,7 @@ function routes() {
       const competitorsList = schema.competitors.all();
 
       let mutatedArray = schema.keywords.all().models.map((model) => {
-        let competitors = Object.keys(
-          competitorsList.models.find(
-            (competitor) => competitor.id === model.id,
-          ).competitor_results,
-        );
+        let competitors = competitorsList.models.find((competitor) => competitor.id === model.id,).competitor_results
         return {
           type: 'keyword',
           id: model.id,
@@ -83,17 +79,23 @@ function routes() {
           },
         };
       });
-      console.log(ENV.mirageConfig.enableRandomErrors)
+
+      const globalCompetitors = Object.keys(schema.competitors.first().competitor_results);
+
+      const allowedColumns = [
+        {name:'Organic position', alias:'position_organic', selected: false, order: 1},
+        {name:'Image places position', alias:'position_places_image', selected: false, order: 2},
+        {name:'Competitor rank', alias:'rank', selected: false, order:3},
+        {name:'Competitor rank type', alias:'rank_type', selected: false, order: 4}]
+
     if (ENV.mirageConfig.enableRandomErrors) {
       if (Math.random() > 0.5) {
         return new Response(500, {}, { errors: ['Server Error'] });
       } else {
-        return { data: mutatedArray };
-
+        return { data: mutatedArray,  meta: {globalCompetitors, allowedColumns} };
       }
     } else {
-      return { data: mutatedArray };
-
+      return { data: mutatedArray, meta: {globalCompetitors, allowedColumns} };
     }
   });
 }
